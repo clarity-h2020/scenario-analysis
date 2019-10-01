38821dbf4ae5ec86bb98c2e48e3286fd33d2b7ad
p-a-s-c-a-l <pascal.dihe@cismet.de>
2019-10-01 17:24:27 +0200

    #12 submit studyInfo object

--- app/scripts/connectors/nodeConnector.js
+++ app/scripts/connectors/nodeConnector.js
@@ -15,19 +15,14 @@ window.Drupal.behaviors.myBehavior = {
         // Using once() to apply the scenarioAnalysisIFrameBehavior effect when you want to run just one function.
         $(window, context).once('scenarioAnalysisIFrameBehavior').each(function () {
             // now using the csisHelpers Module (https://github.com/clarity-h2020/csis-helpers-module)
-            // to get node and group id!
-            var groupId = drupalSettings.csisHelpers.entityinfo.study;
-            var nodeId = drupalSettings.csisHelpers.entityinfo.step;
-            var studyUuid = drupalSettings.csisHelpers.entityinfo.study_uuid;
-            var stepUuid = drupalSettings.csisHelpers.entityinfo.step_uuid;
-            var eventData = {
-                nodeId: nodeId,
-                groupId: groupId,
-                stepUuid: stepUuid,
-                studyUuid: studyUuid
-            };
 
-            console.log('groupId = ' + groupId + ', nodeId = ' + nodeId + ', studyUuid = ' + studyUuid + ', stepUuid = ' + stepUuid);
+            var studyInfo;
+            if (undefined !== drupalSettings && undefined !== drupalSettings.csisHelpers && undefined !== drupalSettings.csisHelpers.studyInfo) {
+                studyInfo = drupalSettings.csisHelpers.studyInfo;
+            } else {
+                console.error('no global csisHelpers object found, probably not connected to Drupal!');
+            }
+
             var connectCount = 0;
             // for some unknown the reason angular directive controler of the embedded child iframe
             // does not recieve the event when it's not fired from the onConnect method.
@@ -36,10 +31,10 @@ window.Drupal.behaviors.myBehavior = {
                             // FIXME: for some unknown reason, onConnect is called if no child frame has actively connected
                             // by invoking seamless.connect() resulting in the onConnect function called twice!
                             if (connectCount === 1) {
-                                scenarioAnalysisApp.send(eventData);
+                                scenarioAnalysisApp.send(studyInfo);
                             } else {
                                 //ignore
-                                //console.log('pre/re connection #:' + connectCount);
+                                console.log('pre/re connection #:' + connectCount);
                             }
                             connectCount++;
                         }});
